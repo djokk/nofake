98c24d988334b3237128822ccd29beea478f530e
JAKHONGIR <52883946+djokk@users.noreply.github.com>
2023-08-20 02:23:03 +0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,8 @@ myElement.style.width = `${document.body.clientWidth}px`;
 myElement.style.height = `${document.body.clientHeight}px`;
 
 function getRandomVelocity() {
+  // return 3; // Случайное значение скорости движения элемента (-3 до 3)
   return (Math.random() - 2) * 3; // Случайное значение скорости движения элемента (-3 до 3)
-}
-
-function getRandomDirection() {
-  return Math.random() < 0.5 ? -1 : 1; // Случайное направление движения элемента (влево или вправо)
 }
 
 function moveElement(element) {
@@ -24,30 +21,26 @@ function moveElement(element) {
   const maxX = document.body.clientWidth - 425; // Максимальная координата по X (ширина окна минус ширина элемента)
   const maxY = document.body.clientHeight - 425; // Максимальная координата по Y (высота окна минус высота элемента)
 
-  // Получаем текущую скорость и направление движения элемента
+  // Получаем текущую скорость элемента
   let velocityX = parseFloat(element.getAttribute('data-velocity-x')) || getRandomVelocity();
   let velocityY = parseFloat(element.getAttribute('data-velocity-y')) || getRandomVelocity();
-  let directionX = parseFloat(element.getAttribute('data-direction-x')) || getRandomDirection();
-  let directionY = parseFloat(element.getAttribute('data-direction-y')) || getRandomDirection();
 
-  // Изменяем координаты элемента на текущую скорость и направление
-  x += velocityX * directionX;
-  y += velocityY * directionY;
+  // Изменяем координаты элемента на текущую скорость
+  x += velocityX;
+  y += velocityY;
 
-  // Отталкиваем элемент от стен с учетом ограничений (-200px)
+  // Отталкиваем элемент от стен с учетом ограничений (-50px)
   if (x <= -200 || x >= maxX + 200) {
-    directionX = -directionX; // Изменяем направление движения в противоположную сторону при столкновении со стеной
+    velocityX = -velocityX; // Изменяем скорость в противоположную сторону при столкновении со стеной
   }
 
   if (y <= -200 || y >= maxY + 200) {
-    directionY = -directionY; // Изменяем направление движения в противоположную сторону при столкновении со стеной
+    velocityY = -velocityY; // Изменяем скорость в противоположную сторону при столкновении со стеной
   }
 
-  // Обновляем скорость и направление движения элемента
+  // Обновляем скорость и координаты элемента
   element.setAttribute('data-velocity-x', velocityX);
   element.setAttribute('data-velocity-y', velocityY);
-  element.setAttribute('data-direction-x', directionX);
-  element.setAttribute('data-direction-y', directionY);
   element.style.left = `${x}px`;
   element.style.top = `${y}px`;
 
@@ -58,4 +51,3 @@ const elements = document.querySelectorAll('.moving-element');
 
 // Запуск анимации для каждого элемента
 elements.forEach(moveElement);
-
